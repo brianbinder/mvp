d4a441e599eea596f3abad7a64ee93c8c5d6f192
Brian <bbinder912@gmail.com>
2017-11-10 16:42:49 -0500

    Make React work

---
+++ client/src/components/app.js
@@ -0,0 +1,11 @@
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App Container">
+        <h1>ReactDom has loaded the App</h1>
+      </div>
+    );
+  }
+}
+
+window.App = App;

3a09457ed89ba237e4c559ebbe448d8a88a22278
Brian <bbinder912@gmail.com>
2017-11-10 19:17:48 -0500

    Submit new users to server

--- client/components/userList.js
+++ client/components/userList.js
@@ -1,13 +1,22 @@
 class UserList extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      newUser: ''
+    };
   }
+
+
+
   render() {
     return (
       <div className="userList">
       {this.props.users.map((user, key) => {
         return <User key={key} user={user}/>
       })}
+      <form action="/submitNewUser" method="post">
+        <input type="text" name="newUserName" /><input type="submit" value="Submit"/>
+      </form>
       </div>
     );
   }
--- server/server.js
+++ server/server.js
@@ -1,13 +1,21 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 
 var port = 1117;
 
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get('/users', function(req, res) {
   var users = ['Tom', 'John', 'Jerry'];
   res.send(users);
+});
+
+app.post('/submitNewUser', function(req, res) {
+  console.log(req.body.newUserName);
+  res.redirect('/');
 });
 
 // app.get('/', function (req, res) {
